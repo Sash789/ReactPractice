1b50e298fe79d671540de1aace22c621baf6f831
Sash789 <saul.soberanesh@gmail.com>
2024-05-30 15:05:30 -0700

    App2 video

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
 
+import React,{useState} from "react";//funcion para poder modificar componentes
+
 function App() {
+  const[resultado, setResultado]= useState(null);// []
+
+
   const elemento = <h1 className="centrar-titulo">Hola alumnos</h1>
-  const elemento2 = suma (7,5);
-  return <><div>{elemento}</div><div>{elemento2}</div></>
-  //return <div><div>{elemento}</div><div>{elemento2}</div></div>
+  const elemento2 = <h2>{suma (7,5)}</h2>
+
+  const botonPulsado =()=>{//Se usa una funcion anonima, no necesariamente
+    const result=suma(7,5);
+    setResultado(result);
+  };
+
+  return <div>
+    <button onClick= {botonPulsado} style={{marginTop: "10px", marginLeft:"10px"}}>Pulsame</button>
+    <div>{elemento}</div>
+    <div>{elemento2}</div>
+    <div>{resultado!=null&& <h2>El resultado es: {resultado}</h2>}</div>{/* resultado!=null&&  SI resultado no es nulo devuelve todo a la derecha de &&  */}
+    </div>
+  //return <><div>{elemento}</div><div>{elemento2}</div></>
 }
+
 
 export default App;
 
